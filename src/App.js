cba257b98a63ebc1802b6b40ce580743bff06834
kdj <rlaeogus0911@nate.com>
2020-04-01 17:20:51 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import Customer from './components/Customer';
+import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table:{
+    minWidth: 1080
+  }
+})
 
 const customers = [{
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
-  'name' : 'kdj1',
+  'name' : 'kdj11',
   'birthday': '23431234',
   'gender' : 'man',
   'job': 'gradument'
@@ -31,26 +49,35 @@ const customers = [{
 
 
 
-function App() {
-  return (
-    <div>
-     {
-      customers.map(c => {
-        return (
-        <Customer  
-          id={c.id}
-          image={c.image}
-          name= {c.name}
-          birthday= {c.birthday}
-          gender={c.gender}
-          job={c.job}
 
-        />
-        )}
-        ) 
-     }
-    </div> 
-  );
+
+class App extends Comment {
+  render(){
+    const { classes} = this.props;
+    return (
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+
+              <TableHead>
+                  <TableRow>
+                    <TableCell>번호</TableCell>
+                    <TableCell>이미지</TableCell>
+                    <TableCell>이름</TableCell>
+                    <TableCell>생년월일</TableCell>
+                    <TableCell>직업</TableCell>
+                    <TableCell>졸업</TableCell>
+                  </TableRow>
+              </TableHead>
+
+              <TableBody>
+                  {customers.map(c=>{ return (<Customer key={c.id} id={c.id} image={c.image} name= {c.name} birthday= {c.birthday} gender={c.gender} job={c.job} /> );
+                  })}
+              </TableBody>
+          </Table>
+        </Paper>
+        
+    ); 
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
